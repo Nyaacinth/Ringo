1207cb2db0707d0208f2f11118efc7fd35c6c5e4
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-01-27 19:01:19 +0800

    喵叫初始化

---
+++ src/MainApp.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js"
+
+export const MainApp: Component = () => {
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import UnoCSS from "unocss/vite"
+import Checker from "vite-plugin-checker"
+import Solid from "vite-plugin-solid"
+
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    server: {
+        port: 1420,
+        strictPort: true
+    },
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+        target: ["es2021", "chrome105", "safari13"]
+    },
+    plugins: [
+        Solid(),
+        UnoCSS(),
+        Checker({
+            typescript: { tsconfigPath: "tsconfig.json" }
+        })
+    ]
+})
