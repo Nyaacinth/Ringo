4bc3665dc4bebcd7ef0f95bb62625c6db7cea3bb
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-01-29 08:23:00 +0800

    Little Fix

--- app/pages/PitchCard.tsx
+++ app/pages/PitchCard.tsx
@@ -9,11 +9,11 @@ export const PitchCard: Component = () => {
 
     return (
         <Card class="w-36 h-36 justify-center items-center">
-            <CardContent class="items-center">
-                <Typography class="text-[#38ADEC]" variant="body2">
+            <CardContent>
+                <Typography class="text-[#38ADEC]" variant="body2" align="center">
                     {Math.round(currentPitchFreq() * 1000) / 1000}
                 </Typography>
-                <Typography class="text-[#1492D5] !fw-bold" variant="h2">
+                <Typography class="text-[#1492D5] !font-bold" variant="h2" align="center">
                     {currentPitchLiteral()}
                 </Typography>
             </CardContent>
